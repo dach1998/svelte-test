e46aa216101cbacf38c70612ba9f91e2faafe753
Danil <dach.king@yandex.ru>
2024-03-06 17:29:26 +0500

    update code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Данил Чижма</title>
+</svelte:head>
+
+<div class="container">
+    <div class="info">
+        <div class="animate-background">
+            <div class="info-text">
+                <h1>
+                    <p class="animate-text-1">Lorem ipsum dolor sit.</p>
+                    <p class="text-2">Lorem ipsum dolor sit amet.</p>
+                </h1>
+            </div>
+            <h2 class="subtitle">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus consequuntur pariatur quisquam, laborum amet illo iusto nesciunt odit quibusdam quaerat, sapiente repudiandae ipsa nobis iure!
+            </h2>
+            <div style="color:black" class="scroll-button animate-scroll" tabindex="0">
+                <a href="/about">Click Here</a>
+                <div class="arrow">&#10145;</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="background-div"></div>
