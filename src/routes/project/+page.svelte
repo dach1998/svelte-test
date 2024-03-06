e91206d5d974e0c2d0998ecca671ae64c1e3797d
Danil <dach.king@yandex.ru>
2024-03-06 19:55:34 +0500

    add projects

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,3 +1,47 @@
-<h1>Портфолио</h1>
+<svelte:head>
+    <title>Портфолио</title>
+    <style>
+        .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
 
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate molestiae repellendus temporibus. Ipsum suscipit, corrupti obcaecati illo accusantium sunt fugiat sint blanditiis unde reiciendis quasi rem nihil ex quia enim.
+    .apps {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+
+    .app {
+        border: 1px solid #ddd;
+        padding: 10px;
+        box-shadow: 0 0 10px #eee;
+    }
+
+    .link {
+        color: rgb(10,10,139);
+    }
+    </style>
+</svelte:head>
+<div class="container">
+    <h1>Мои проекты</h1>
+    <div class="apps">
+        {#each routes as page}
+        <div class="app">
+            <h2>{page.title}</h2>
+            <p>{page.body}</p>
+            <p class="readmore">
+                <a href="{`/project/apps/${page.id}`}" class="link">
+                    Читать далее
+                </a>
+            </p>
+        </div>
+        {/each}
+    </div>
+</div>
+
+
+<script>
+    import {routes} from './routes'
+</script>
