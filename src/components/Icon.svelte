4257a110a3b6c0a8d8c8a889db941c981935e323
Danil <dach.king@yandex.ru>
2024-03-07 11:18:44 +0500

    .

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,5 +1,6 @@
 <script>
     export let name;
+
     const icons = [
         {
             name: 'check-mark',
@@ -10,6 +11,7 @@
             path: 'M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z',
         },
     ];
+
     let icon = icons.find((item) => item.name === name);
 </script>
 
